5c1da30d182cf1b1e811c06dfc4e5d3a61485079
y-hiroto2025 <hiro.ymn.15233277@gmail.com>
2025-12-04 15:36:57 +0900

    4-data_visualize Reviewed By Gemini

--- 4-data_visualize.py
+++ 4-data_visualize.py
@@ -5,8 +5,14 @@ import matplotlib.pyplot as plt
 
 datas = np.random.normal(loc=50, scale=10, size=1000)
 
-plt.hist(x=datas, bins=30)
+plt.hist(x=datas, bins=30, alpha=0.7, label="Attack Damage") # 改善点：alphaで少し透けさせる、ラベルを付ける
+
+plt.title("Damage Distribution") # 改善点：タイトル追加
 plt.xlabel("Damages")
 plt.ylabel("Count")
-plt.axvline(x=60, color="red") # ここからクリティカルダメージ
+
+# クリティカルライン
+plt.axvline(x=60, color="red", linestyle="--") # 改善点：点線にする
+
+plt.legend() # 改善点：凡例を表示
 plt.show()
--- 5-clean_data.py
+++ 5-clean_data.py
@@ -1,4 +1,12 @@
-# 余計な記号の入った文字列データをきれいにする
-
+# 余計な記号や文字列の入った文字列データをきれいにする
+# １：ユーザー名のリストを作る
+# ２：タグを削除してきれいなテキストを取り出す
 import re
+
+log_data = """
+[INFO] User:Yamada Message:ご飯食べたい
+[ERROR] Code:404 User:Asuna Message:リンク切れです <a href="http://あああ.com">Link</a>
+[INFO] User:Mori Message:ピザ食べたい... map_id:55
+[WARNING] User:Agil Message:在庫が足りない (item_id: 302)
+"""
 
